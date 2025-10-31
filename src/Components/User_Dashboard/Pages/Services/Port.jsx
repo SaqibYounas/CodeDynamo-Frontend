87e3b516d1605d2a213a3895a45fc4edcadc83a0
Saqib Younas <muhammadsaqibyounas11@gmail.com>
2025-11-01 00:15:27 +0500

    ...

--- src/Components/User_Dashboard/Pages/Services/Port.jsx
+++ src/Components/User_Dashboard/Pages/Services/Port.jsx
@@ -1,3 +1,15 @@
-const port = 8003;
-const baseUrl = import.meta.env.VITE_URL;
-export const url = `${baseUrl}:${port}`;
+import { io } from "socket.io-client";
+
+let socket;
+
+if (import.meta.env.VITE_URL) {
+  const baseUrl = import.meta.env.VITE_URL;
+  const port = import.meta.env.VITE_PORT;
+  const url = port ? `${baseUrl}:${port}` : baseUrl;
+
+  socket = io(url);
+} else {
+  console.log("Backend not deployed yet, Socket.IO disabled");
+}
+
+export default socket;
